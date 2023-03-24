a1e5f85f6da73be440cd2fc599e14d0a77386b77
trungle2021 <leanhtrung97@gmail.com>
2023-03-24 13:33:03 +0700

    update calendar

--- app/src/main/java/com/example/wmandroid/OrderCalendarActivity.java
+++ app/src/main/java/com/example/wmandroid/OrderCalendarActivity.java
@@ -1,21 +1,30 @@
 package com.example.wmandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.example.wmandroid.databinding.ActivityOrderCalendarBinding;
+import android.util.Log;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class OrderCalendarActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    TextView date;
 
-    ActivityOrderCalendarBinding calendarBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        calendarBinding = ActivityOrderCalendarBinding.inflate(getLayoutInflater());
-        setContentView(calendarBinding.getRoot());
-
-
-
+        setContentView(R.layout.activity_order_calendar);
+        calendarView = findViewById(R.id.calendarView);
+        date = findViewById(R.id.date);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String todayDate = month + "/" + dayOfMonth + "/" + year;
+                Log.d("date",todayDate);
+                date.setText(todayDate);
+            }
+        });
     }
 }
