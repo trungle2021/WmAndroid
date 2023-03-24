4ef5bb5721661745b919cd167a7ce83e058913b8
trungle2021 <leanhtrung97@gmail.com>
2023-03-24 15:06:29 +0700

    Order Calendar UI

--- app/src/main/java/com/example/wmandroid/OrderCalendarActivity.java
+++ app/src/main/java/com/example/wmandroid/OrderCalendarActivity.java
@@ -7,23 +7,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderCalendarActivity extends AppCompatActivity {
     CalendarView calendarView;
-    TextView date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_calendar);
         calendarView = findViewById(R.id.calendarView);
-        date = findViewById(R.id.date);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String todayDate = month + "/" + dayOfMonth + "/" + year;
                 Log.d("date",todayDate);
-                date.setText(todayDate);
+                Toast.makeText(OrderCalendarActivity.this, todayDate, Toast.LENGTH_SHORT).show();
             }
         });
     }
