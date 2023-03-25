ea5c64779d75d8f6f05eba09c249f2c50ae557f1
le anh trung <leanhtrung97@gmail.com>
2023-03-25 15:46:38 +0700

    fix apiclient

--- app/src/main/java/com/example/wmandroid/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/wmandroid/Fragment/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.wmandroid.Fragment;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,15 +9,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Lifecycle;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.wmandroid.API.ApiClient;
+import com.example.wmandroid.API.Auth.AuthService;
+import com.example.wmandroid.API.VenueService;
+import com.example.wmandroid.DTO.VenueImgDTO;
+import com.example.wmandroid.NavigateActivity;
 import com.example.wmandroid.R;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class ProfileFragment extends Fragment {
     View view;
+    ApiClient apiClient;
+
 
     public ProfileFragment() {
     }
@@ -26,6 +42,7 @@ public class ProfileFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
         return view;
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
