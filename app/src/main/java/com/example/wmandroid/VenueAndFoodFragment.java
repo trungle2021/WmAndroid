47122290a7f44da04479226a44a443b867acabaf
khoa954 <87458205+khoa954@users.noreply.github.com>
2023-03-25 14:16:12 +0700

    add fragmen( chưa test)

---
+++ app/src/main/java/com/example/wmandroid/VenueAndFoodFragment.java
@@ -0,0 +1,132 @@
+package com.example.wmandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wmandroid.API.ApiClient;
+import com.example.wmandroid.API.MenuService;
+import com.example.wmandroid.API.VenueService;
+import com.example.wmandroid.Adapter.MenuAdapter;
+import com.example.wmandroid.Adapter.VenueAdapter;
+import com.example.wmandroid.DTO.FoodImageDTO;
+import com.example.wmandroid.DTO.VenueImgDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link VenueAndFoodFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VenueAndFoodFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    VenueAdapter venueAdapter;
+    MenuAdapter menuAdapter;
+    List<VenueImgDTO> venueImgDTOList=new ArrayList<>();
+    List<FoodImageDTO> foodImageDTOS=new ArrayList<>();
+    ApiClient apiClient = new ApiClient(getActivity());
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public VenueAndFoodFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VenueAndFoodFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VenueAndFoodFragment newInstance(String param1, String param2) {
+        VenueAndFoodFragment fragment = new VenueAndFoodFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_venue_and_food, container, false);
+        RecyclerView recyclerViewVenue=view.findViewById(R.id.recycleViewVenue);
+        RecyclerView recyclerViewMenu=view.findViewById(R.id.recycleViewMenu);
+        recyclerViewVenue.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerViewMenu.setLayoutManager(new LinearLayoutManager(getActivity()));
+        initControl();
+        callAPiMenuImg();
+        callApiVenueImg();
+        return view;
+    }
+    private void callAPiMenuImg() {
+        MenuService menuService=apiClient.createService(MenuService.class);
+        menuService.foodImgAll().enqueue(new Callback<List<FoodImageDTO>>() {
+            @Override
+            public void onResponse(Call<List<FoodImageDTO>> call, Response<List<FoodImageDTO>> response) {
+                foodImageDTOS.clear();
+                foodImageDTOS.addAll(response.body());
+                menuAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<FoodImageDTO>> call, Throwable t) {
+                Log.d("Venue & Food fragment","Get All Food Img");
+            }
+        });
+    }
+    private void callApiVenueImg() {
+        VenueService venueService = apiClient.createService(VenueService.class);
+        venueService.venueImgAll().enqueue(new Callback<List<VenueImgDTO>>() {
+            @Override
+            public void onResponse(Call<List<VenueImgDTO>> call, Response<List<VenueImgDTO>> response) {
+                venueImgDTOList.clear();
+                venueImgDTOList.addAll(response.body());
+                venueAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onFailure(Call<List<VenueImgDTO>> call, Throwable t) {
+                Log.d("Venue & Food fragment","Get All Venue Img");
+            }
+        });
+    }
+    private void initControl() {
+        venueAdapter = new VenueAdapter(getActivity());
+        menuAdapter = new MenuAdapter(getActivity());
+        venueAdapter.setVenueData(venueImgDTOList);
+        menuAdapter.setMenuData(foodImageDTOS);
+
+    }
+}
